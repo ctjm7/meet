f965323ba355bf5919cc98b8545ec5a8d90e7883
ctjm7 <107513125+ctjm7@users.noreply.github.com>
2022-10-20 12:39:57 -0700

    added end to end tests for filter events by city

--- src/_tests_/EndToEnd.test.js
+++ src/_tests_/EndToEnd.test.js
@@ -39,3 +39,46 @@ describe('show/hide an event details', () => {
     expect(eventDetails).toBeNull();
   });
 });
+
+describe('Filter events by city', () => {
+  jest.setTimeout(30000);
+  let browser;
+  let page;
+  beforeAll(async () => {
+    jest.setTimeout(30000);
+    browser = await puppeteer.launch({
+     headless: false,
+      // slow down by 250ms
+      slowMo: 250,
+      // ignores default setting that causes timeout errors
+      ignoreDefaultArgs: ['--disable-extensions']
+    });
+    page = await browser.newPage();
+    await page.goto('http://localhost:3000/');
+    await page.waitForSelector('.event');
+  });
+
+  afterAll(() => {
+    browser.close();
+  });
+  test('When user hasn’t searched for a city, show upcoming events from all cities.', async () => {
+    const numberEvents = await page.$$eval('.event', (element) => element.length);
+      expect(numberEvents).toBe(2);
+  });
+
+  test('User should see a list of suggestions when they search for a city', async () => {
+    await page.type('.city', 'Berlin');
+    const numberCities = await page.$$eval('.suggestions li', (element) =>
+      element.length);
+    expect(numberCities).toBe(2);
+  });
+
+  test('User can select a city from the suggested list', async () => {
+    await page.reload();
+    await page.type('.city', 'Berlin');
+    await page.click('.suggestions li');
+    const numberEvents = await page.$$eval('.event', (element) =>
+      element.length);
+      expect(numberEvents).toBe(1);
+  });
+});
