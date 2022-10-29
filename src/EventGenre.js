43e369467d5e0a4d8e8b99974f15e9fb811b0e3c
ctjm7 <107513125+ctjm7@users.noreply.github.com>
2022-10-28 19:08:38 -0700

    fixed piechart

--- src/EventGenre.js
+++ src/EventGenre.js
@@ -6,24 +6,26 @@ const EventGenre = ({ events }) => {
   const colors = ["#00ae88", "#ee82ee", "#00aee0", "#c0aee0" ]
 
   useEffect(() => {
+    const getData = () => {
+      const genres = ['React', 'JavaScript', 'Node', 'jQuery', 'AngularJS'];
+      const data = genres.map((genre) => {
+        const value = events.filter((event) =>
+          event.summary.split(' ').includes(genre)).length;
+        return { name: genre, value };
+      });
+    return data;
+    };
     setData(() => getData());
   }, [events]);
 
-  const getData = () => {
-    const genres = ['React', 'JavaScript', 'Node', 'jQuery', 'AngularJS'];
-    const data = genres.map((genre) => {
-      const value = events.filter(event => event.split(' ').includes(genre)).length;
-      return { name: genre, value };
-    });
-    return data;
-  };
+
 
   return (
     <ResponsiveContainer height={400}>
       <PieChart width={400} height={400}>
         <Pie
           data={data}
-          cs={200}
+          cx={200}
           cy={200}
           labelLine={false}
           outerRadius={80}
